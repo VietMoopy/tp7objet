70b72c056cb51bb51a1681e62fab1344a0a8dfcc
alexandrenguyen68817 <alexandrenguyen68817@gmail.com>
2016-05-25 04:09:26 -0400

    Creation de contact.php et ajout de l'attribut email

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+class Contact {
+	protected $id;
+	protected $nom;
+	protected $prenom;
+  protected $email;
+  
+  function __construct($id,$nom,$prenom,$email){
+    $this->id = $id;
+    $this->nom = $nom;
+    $this->prenom = $prenom;
+    $this->email = $email;
+  }
+  
+  public function getNom(){
+    return $this->nom;
+  }
+  
+  public function getPrenom(){
+    return $this->prenom;
+  }
+  
+  public function setNom($nom){
+    $this->nom = $nom;
+  }
+  
+  public function setPrenom($prenom){
+    $this->prenom = $prenom;
+  }
+  
+  function __toString(){
+    return "id = ".$this->id." nom = ".$this->nom." prenom = ".$this->prenom." email = ".$this->email;
+  }
+  
+}
+
+$me = new Contact(1234,"Nguyen","Alex","alex@gmail.com");
+echo $me->__toString();
+?>
